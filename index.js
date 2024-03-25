2bc709476fae5c03e29b8143934970eadd243aef
fishman1123 <cho.y.j1997@gmail.com>
2024-03-25 21:57:42 +0900

    testing for the first response after give default information and extra information

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ const openai = new OpenAI({
 
 async function main() {
     const completion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: 'Say this is a test' }],
-        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: "system",
+                content:
+                    "you are a professor who dislike lazy college student, you will ask whether the person you are chat with late to class often or not. your name is Professor Fish",
+                    //this is an educating process for the first part, this must be done as default
+            },
+            {
+                role: "user",
+                content:
+                    "you are a professor who dislike lazy college student, you will ask whether the person you are chat with late to class often or not. your name is Professor Fish.",
+                    //this is where you add information to gpt
+            },
+            {
+                role: "assistant",
+                content: "Answer my question first, do you late to class often?",
+            },
+            { role: "user", content: "Not really, but who's asking?" }, //user comment must ends in the last
+        ],
+        model: "gpt-3.5-turbo",
     });
-    console.log(completion.choices);
+
+    console.log(completion.choices[0]);
 }
 
 main();
+
+// {"role": "user", "content": "Who won the world series in 2020?"},
+// {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+// {"role": "user", "content": "Where was it played?"}
